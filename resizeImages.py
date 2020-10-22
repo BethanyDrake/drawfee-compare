ad8d871ba2f97771512cb5563dc2da2a76373ec8
Bethany Drake <bethanyd@thoughtworks.com>
2020-10-22 11:39:17 +1100

    add more images

--- resizeImages.py
+++ resizeImages.py
@@ -4,27 +4,37 @@ from resizeimage import resizeimage
 
 print("hello")
 
-images = [
-'src/images/1a.png',
-'src/images/1b.png',
-'src/images/1c.png',
-'src/images/2a.png',
-'src/images/2b.png',
-'src/images/2c.png',
-'src/images/2d.png',
-'src/images/3a.png',
-'src/images/3b.png',
-'src/images/3c.png',
-'src/images/4a.png',
-'src/images/4b.png',
-'src/images/4c.png',
-]
+images ='''src/images/5a.png
+src/images/5b.png
+src/images/5c.png
+src/images/5d.png
+src/images/6a.png
+src/images/6b.png
+src/images/6c.png
+src/images/6d.png
+src/images/7a.png
+src/images/7b.png
+src/images/7c.png
+src/images/8a.png
+src/images/8b.png
+src/images/8c.png
+src/images/8d.png
+src/images/9a.png
+src/images/9b.png
+src/images/9c.png
+src/images/9d.png
+src/images/10a.png
+src/images/10b.png
+src/images/10c.png
+src/images/10d.png
+'''.splitlines()
+
 
 for path in images:
 
   with open(path, 'r+b') as f:
       with Image.open(f) as image:
-          cover = resizeimage.resize_cover(image, [250, 200])
+          cover = resizeimage.resize_cover(image, [500, 400])
           cover.save(path, image.format)
           print("done: "+path)
 
--- src/MatchupService.js
+++ src/MatchupService.js
@@ -65,6 +65,128 @@ const images = [
     id: "4c",
     videoTitle: "Artists Draw Gigantamax Pokémon From Memory",
     videoUrl: "https://www.youtube.com/watch?v=5CIeVKO2lCk"
+  },
+  {
+    id: "5a",
+    videoTitle: "Artists Play Celebrity Pictionary",
+    videoUrl: "https://www.youtube.com/watch?v=qOJXy45REks"
+  },
+  {
+    id: "5b",
+    videoTitle: "Artists Play Celebrity Pictionary",
+    videoUrl: "https://www.youtube.com/watch?v=qOJXy45REks"
+  },
+  {
+    id: "5c",
+    videoTitle: "Artists Play Celebrity Pictionary",
+    videoUrl: "https://www.youtube.com/watch?v=qOJXy45REks"
+  },
+  {
+    id: "5d",
+    videoTitle: "Artists Play Celebrity Pictionary",
+    videoUrl: "https://www.youtube.com/watch?v=qOJXy45REks"
+  },
+  {
+    id: "6a",
+    videoTitle: "Artists Draw Personalized Tarot Cards",
+    videoUrl: "https://www.youtube.com/watch?v=TRn7zrkmA00"
+  },
+  {
+    id: "6b",
+    videoTitle: "Artists Draw Personalized Tarot Cards",
+    videoUrl: "https://www.youtube.com/watch?v=TRn7zrkmA00"
+  },
+  {
+    id: "6c",
+    videoTitle: "Artists Draw Personalized Tarot Cards",
+    videoUrl: "https://www.youtube.com/watch?v=TRn7zrkmA00"
+  },
+  {
+    id: "6d",
+    videoTitle: "Artists Draw Personalized Tarot Cards",
+    videoUrl: "https://www.youtube.com/watch?v=TRn7zrkmA00"
+  },
+  {
+    id: "7a",
+    videoTitle:
+      "Artists Draw Even More Bloodborne Bosses (That They've Never Seen)",
+    videoUrl: "https://www.youtube.com/watch?v=-5q1yxRni28"
+  },
+  {
+    id: "7b",
+    videoTitle:
+      "Artists Draw Even More Bloodborne Bosses (That They've Never Seen)",
+    videoUrl: "https://www.youtube.com/watch?v=-5q1yxRni28"
+  },
+  {
+    id: "7c",
+    videoTitle:
+      "Artists Draw Even More Bloodborne Bosses (That They've Never Seen)",
+    videoUrl: "https://www.youtube.com/watch?v=-5q1yxRni28"
+  },
+  {
+    id: "8a",
+    videoTitle: "30 Minute Speed Draw CHALLENGE",
+    videoUrl: "https://www.youtube.com/watch?v=yxmW1XqDrXk"
+  },
+  {
+    id: "8b",
+    videoTitle: "30 Minute Speed Draw CHALLENGE",
+    videoUrl: "https://www.youtube.com/watch?v=yxmW1XqDrXk"
+  },
+  {
+    id: "8c",
+    videoTitle: "30 Minute Speed Draw CHALLENGE",
+    videoUrl: "https://www.youtube.com/watch?v=yxmW1XqDrXk"
+  },
+  {
+    id: "8d",
+    videoTitle: "30 Minute Speed Draw CHALLENGE",
+    videoUrl: "https://www.youtube.com/watch?v=yxmW1XqDrXk"
+  },
+  {
+    id: "9a",
+    videoTitle: "Artists Draw Pokémon (Based Only On Their Names)",
+    videoUrl: "https://www.youtube.com/watch?v=M9VzE4t2dJk"
+  },
+  {
+    id: "9b",
+    videoTitle: "Artists Draw Pokémon (Based Only On Their Names)",
+    videoUrl: "https://www.youtube.com/watch?v=M9VzE4t2dJk"
+  },
+  {
+    id: "9c",
+    videoTitle: "Artists Draw Pokémon (Based Only On Their Names)",
+    videoUrl: "https://www.youtube.com/watch?v=M9VzE4t2dJk"
+  },
+  {
+    id: "9d",
+    videoTitle: "Artists Draw Pokémon (Based Only On Their Names)",
+    videoUrl: "https://www.youtube.com/watch?v=M9VzE4t2dJk"
+  },
+  {
+    id: "10a",
+    videoTitle:
+      "Artists Draw EVEN MORE Celebrities (Based Only On Description)",
+    videoUrl: "https://www.youtube.com/watch?v=azyBXITLOrY"
+  },
+  {
+    id: "10b",
+    videoTitle:
+      "Artists Draw EVEN MORE Celebrities (Based Only On Description)",
+    videoUrl: "https://www.youtube.com/watch?v=azyBXITLOrY"
+  },
+  {
+    id: "10c",
+    videoTitle:
+      "Artists Draw EVEN MORE Celebrities (Based Only On Description)",
+    videoUrl: "https://www.youtube.com/watch?v=azyBXITLOrY"
+  },
+  {
+    id: "10d",
+    videoTitle:
+      "Artists Draw EVEN MORE Celebrities (Based Only On Description)",
+    videoUrl: "https://www.youtube.com/watch?v=azyBXITLOrY"
   }
 ];
 
